66aeabd98421b081eadb91236aeade9836dec202
신용 <ksy8872038@gmail.com>
2025-11-21 09:35:21 +0900

    초기 세팅

---
+++ app.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+import postsRouter from "./router/post.mjs";
+import authRouter from "./router/auth.mjs";
+
+const app = express();
+
+app.use(express.json()); // json으로 통신하도록 미들웨어에 등록
+
+app.use("/post", postsRouter); // /post로 접근
+app.use("/auth", authRouter);
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+app.listen(8080); // port번호 지정
