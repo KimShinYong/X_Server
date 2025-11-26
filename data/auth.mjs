4ad12aef4c92aa6eda012b86a4e75ea1c38e95d4
신용 <ksy8872038@gmail.com>
2025-11-26 11:32:20 +0900

    mongoose 적용완료

--- data/auth.mjs
+++ data/auth.mjs
@@ -1,44 +1,32 @@
 import MongoDB from "mongodb";
-import { getUsers } from "../db/database.mjs";
-const ObjectID = MongoDB.ObjectId;
+import { useVirtualId } from "../db/database.mjs";
+import Mongoose from "mongoose";
+
+const userSchema = new Mongoose.Schema(
+  {
+    userid: { type: String, require: true },
+    name: { type: String, require: true },
+    email: { type: String, require: true },
+    password: { type: String, require: true },
+    url: String,
+  },
+  { versionKey: false } // veirsionKey: Mongoose가 문서를 저장할 때 자동으로 추가하는 _v라는 필드를 설정
+);
+
+useVirtualId(userSchema);
+const User = Mongoose.model("User", userSchema);
 
 // 회원가입
 export async function createUser(user) {
-  // const user = {
-  //   id: Date.now().toString(),
-  //   userid,
-  //   password,
-  //   name,
-  //   email,
-  //   url: "https://randomuser.me/api/portraits/men/29.jpg",
-  // };
-  // users = [user, ...users];
-  // return user
-  return getUsers()
-    .insertOne(user)
-    .then((result) => result.insertedId.toString());
+  return new User(user).save().then((data) => data.id); // insert
 }
-
-// 로그인
-// export async function login(userid, password) {
-//   return users.find(
-//     (user) => user.userid === userid && user.password === password
-//   );
-// }
 
 // 회원 체크
 export async function findByUserid(userid) {
-  return getUsers().find({ userid }).next().then(mapOptionalUser);
+  return User.findOne({ userid });
 }
 
 // ID로 찾기
 export async function findById(id) {
-  return getUsers()
-    .find({ _id: new ObjectID(id) })
-    .next()
-    .then(mapOptionalUser);
-}
-
-function mapOptionalUser(user) {
-  return user ? { ...user, id: user._id.toString() } : user;
+  return User.findById(id);
 }
